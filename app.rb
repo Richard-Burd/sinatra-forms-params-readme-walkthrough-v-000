46e483cd6d12f1a0fe59601134cefa43f48f15f3
Richard Burd <richardburdx@yahoo.com>
2017-11-30 03:07:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,4 +9,14 @@ class App < Sinatra::Base
   post '/food' do
     "My name is #{params[:this_shit_transfers]}, and I love #{params[:fuck_shit]}"
   end
+
+  # OK these are my custom code snippets
+  get '/burd_page' do
+    erb :burd_page
+  end
+
+  post '/burd_answers' do
+    params.to_s
+    erb :burd_answers
+  end
 end
