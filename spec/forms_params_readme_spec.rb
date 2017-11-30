583a23131246cdc2d822e9cc08fef8d15052a270
Richard Burd <richardburdx@yahoo.com>
2017-11-30 03:38:13 +0000

    automatically backed up by learn

--- spec/forms_params_readme_spec.rb
+++ spec/forms_params_readme_spec.rb
@@ -27,15 +27,15 @@ describe "App" do
       expect(last_response.status).to eq(200)
     end
 
-    #it 'displays the params' do
-    #  params = {
-    #    :name => "Carl",
-    #    :favorite_food => "fried chicken"
-    #  }
+    it 'displays the params' do
+      params = {
+        :first_param => "Heroditus",
+        :second_param => "Triclenaum"
+      }
 
-    #  post '/food', params
+      post '/burd_answers', params
 
-    #  expect(last_response.body).to eq("My name is #{params[:name]}, and I love #{params[:favorite_food]}")
-    #end
+      expect(last_response.body).to eq("My name is #{params[:first_param]}, and I love #{params[:second_param]}")
+    end
   end
 end
