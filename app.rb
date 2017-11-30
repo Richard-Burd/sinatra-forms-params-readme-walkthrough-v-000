45e4f93d4430ae717e06898e1e7a30bf1a1d5c76
Richard Burd <richardburdx@yahoo.com>
2017-11-30 03:25:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/food' do
-    "My name is #{params[:name]}, and I love #{params[:favorite_foods]}"
+    "My name is #{params[:this_shit_transfers]}, and I love #{params[:fuck_shit]}"
   end
 
   # OK these are my custom code snippets
